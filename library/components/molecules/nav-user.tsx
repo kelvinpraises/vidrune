e94844547b0991535d7479e8fd953e453c1efa94
kelvinpraises <kelvinpraises@gmail.com>
2024-12-18 05:05:48 +0100

    feat: add theme changer

--- library/components/molecules/nav-user.tsx
+++ library/components/molecules/nav-user.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useModal } from "connectkit";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 import { useCallback } from "react";
 import {
   useAccount,
@@ -9,6 +11,7 @@ import {
   useEnsName,
 } from "wagmi";
 
+import { Button } from "@/components/atoms/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -27,6 +30,7 @@ export function UserNav() {
   const { disconnect } = useDisconnect();
   const { reset } = useConnect$1();
   const { setOpen } = useModal();
+  const { setTheme } = useTheme();
   const { data: ensName } = useEnsName({
     chainId: chainId,
     address: address,
@@ -40,48 +44,92 @@ export function UserNav() {
 
   if (!isConnected && !address) {
     return (
-      <button
-        onClick={() => setOpen(true)}
-        disabled={!isMounted}
-        className={cn(
-          "bg-[#33CB82] hover:bg-[#33CB82]/80 font-medium px-6 rounded-[0] py-4 transition-colors duration-200",
-          !isMounted && "opacity-20 cursor-not-allowed"
-        )}
-      >
-        Connect Wallet
-      </button>
+      <div className="flex items-center gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <button
+          onClick={() => setOpen(true)}
+          disabled={!isMounted}
+          className={cn(
+            "bg-[#33CB82] hover:bg-[#33CB82]/80 font-medium px-6 rounded-[0] py-4 transition-colors duration-200",
+            !isMounted && "opacity-20 cursor-not-allowed"
+          )}
+        >
+          Connect Wallet
+        </button>
+      </div>
     );
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button className="bg-[#33CB82] hover:bg-[#33CB82]/80 font-medium px-6 rounded-[0] py-4 transition-colors duration-200">
-          {ensName || ellipsisAddress(address || "")}
-        </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Connected</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {chain?.name}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem onClick={() => setOpen(true)}>
-            Profile
+    <div className="flex items-center gap-4">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
           </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem onClick={handleDisconnect}>
-            Disconnect
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
           </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="bg-[#33CB82] hover:bg-[#33CB82]/80 font-medium px-6 rounded-[0] py-4 transition-colors duration-200">
+            {ensName || ellipsisAddress(address || "")}
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">Connected</p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {chain?.name}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={() => setOpen(true)}>
+              Profile
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={handleDisconnect}>
+              Disconnect
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
