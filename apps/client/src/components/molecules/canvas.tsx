d0a54ff41f479d936e71db5047e524f9ffcd97ba
kelvinpraises <kelvinpraises@gmail.com>
2025-12-08 03:19:16 +0100

    fix: downgrade to React 18 and fix wagmi/viem compatibility

--- apps/client/src/components/molecules/canvas.tsx
+++ apps/client/src/components/molecules/canvas.tsx
@@ -1,11 +1,11 @@
 import type { JSX } from "react";
 
 interface CanvasProps extends React.CanvasHTMLAttributes<HTMLCanvasElement> {
-  canvasRef: React.RefObject<HTMLCanvasElement | null>;
+  canvasRef: React.RefObject<HTMLCanvasElement>;
 }
 
 const Canvas = ({ canvasRef, ...props }: CanvasProps): JSX.Element => {
-  return <canvas hidden ref={canvasRef} {...props} />;
+  return <canvas hidden ref={canvasRef as React.LegacyRef<HTMLCanvasElement>} {...props} />;
 };
 
 export default Canvas;
